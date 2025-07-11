8ef7cfdab8452fa0ecd9eb4907481f27f64ab401
Yann <yannmasson17@gmail.com>
2025-07-11 16:19:15 +0200

    feat: add auto updated to the electron app

--- electron/main.ts
+++ electron/main.ts
@@ -1,10 +1,12 @@
-import { app, BrowserWindow } from 'electron'
-// import { createRequire } from 'node:module'
-import { fileURLToPath } from 'node:url'
-import path from 'node:path'
+import {app, BrowserWindow} from 'electron';
+import electronUpdater, {type AppUpdater} from 'electron-updater';
+import {fileURLToPath} from 'node:url';
+import path from 'node:path';
+
+// import { createRequire } from 'node:module'*
 
 // const require = createRequire(import.meta.url)
-const __dirname = path.dirname(fileURLToPath(import.meta.url))
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 // The built directory structure
 //
@@ -15,54 +17,103 @@ const __dirname = path.dirname(fileURLToPath(import.meta.url))
 // │ │ ├── main.js
 // │ │ └── preload.mjs
 // │
-process.env.APP_ROOT = path.join(__dirname, '..')
+process.env.APP_ROOT = path.join(__dirname, '..');
 
 // 🚧 Use ['ENV_NAME'] avoid vite:define plugin - Vite@2.x
-export const VITE_DEV_SERVER_URL = process.env['VITE_DEV_SERVER_URL']
-export const MAIN_DIST = path.join(process.env.APP_ROOT, 'dist-electron')
-export const RENDERER_DIST = path.join(process.env.APP_ROOT, 'dist')
+export const VITE_DEV_SERVER_URL = process.env['VITE_DEV_SERVER_URL'];
+export const MAIN_DIST = path.join(process.env.APP_ROOT, 'dist-electron');
+export const RENDERER_DIST = path.join(process.env.APP_ROOT, 'dist');
 
-process.env.VITE_PUBLIC = VITE_DEV_SERVER_URL ? path.join(process.env.APP_ROOT, 'public') : RENDERER_DIST
+process.env.VITE_PUBLIC = VITE_DEV_SERVER_URL ? path.join(process.env.APP_ROOT, 'public') : RENDERER_DIST;
 
-let win: BrowserWindow | null
+let win: BrowserWindow | null;
 
 function createWindow() {
-  win = new BrowserWindow({
-    icon: path.join(process.env.VITE_PUBLIC, 'electron-vite.svg'),
-    webPreferences: {
-      preload: path.join(__dirname, 'preload.mjs'),
-    },
-  })
+    win = new BrowserWindow({
+        icon: path.join(process.env.VITE_PUBLIC, 'electron-vite.svg'),
+        webPreferences: {
+            preload: path.join(__dirname, 'preload.mjs'),
+        },
+    });
 
-  // Test active push message to Renderer-process.
-  win.webContents.on('did-finish-load', () => {
-    win?.webContents.send('main-process-message', (new Date).toLocaleString())
-  })
+    // Test active push message to Renderer-process.
+    win.webContents.on('did-finish-load', () => {
+        win?.webContents.send('main-process-message', (new Date).toLocaleString());
+    });
 
-  if (VITE_DEV_SERVER_URL) {
-    win.loadURL(VITE_DEV_SERVER_URL)
-  } else {
-    // win.loadFile('dist/index.html')
-    win.loadFile(path.join(RENDERER_DIST, 'index.html'))
-  }
+    if (VITE_DEV_SERVER_URL) {
+        win.loadURL(VITE_DEV_SERVER_URL);
+    } else {
+        // win.loadFile('dist/index.html')
+        win.loadFile(path.join(RENDERER_DIST, 'index.html'));
+    }
+
+    if (!VITE_DEV_SERVER_URL) {
+        setupAutoUpdater();
+    }
+}
+
+export function getAutoUpdater(): AppUpdater {
+    // Using destructuring to access autoUpdater due to the CommonJS module of 'electron-updater'.
+    // It is a workaround for ESM compatibility issues, see https://github.com/electron-userland/electron-builder/issues/7976.
+    const {autoUpdater} = electronUpdater;
+    return autoUpdater;
+}
+
+function setupAutoUpdater() {
+    const autoUpdater = getAutoUpdater();
+
+    autoUpdater.autoDownload = false;
+    autoUpdater.autoInstallOnAppQuit = true;
+
+    autoUpdater.on('checking-for-update', () => {
+        console.log('Checking for updates...');
+    });
+
+    autoUpdater.on('update-available', (info) => {
+        console.log('Update available:', info.version);
+        autoUpdater.downloadUpdate().then();
+    });
+
+    autoUpdater.on('update-not-available', (info) => {
+        console.log('No update available:', info.version);
+    });
+
+    autoUpdater.on('error', (err) => {
+        console.error('Error during update:', err);
+    });
+
+    autoUpdater.on('download-progress', (progressObj) => {
+        let log_message = "Download speed: " + progressObj.bytesPerSecond;
+        log_message = log_message + ' - Downloaded ' + progressObj.percent + '%';
+        log_message = log_message + ' (' + progressObj.transferred + "/" + progressObj.total + ')';
+        console.log(log_message);
+    });
+
+    autoUpdater.on('update-downloaded', (info) => {
+        console.log('Update downloaded:', info.version);
+        autoUpdater.quitAndInstall();
+    });
+
+    autoUpdater.checkForUpdatesAndNotify().catch(err => {
+        console.error('Error while checking for updates:', err);
+    });
 }
 
 // Quit when all windows are closed, except on macOS. There, it's common
 // for applications and their menu bar to stay active until the user quits
 // explicitly with Cmd + Q.
 app.on('window-all-closed', () => {
-  if (process.platform !== 'darwin') {
-    app.quit()
-    win = null
-  }
-})
+    if (process.platform !== 'darwin') {
+        app.quit();
+        win = null;
+    }
+});
 
 app.on('activate', () => {
-  // On OS X it's common to re-create a window in the app when the
-  // dock icon is clicked and there are no other windows open.
-  if (BrowserWindow.getAllWindows().length === 0) {
-    createWindow()
-  }
-})
+    if (BrowserWindow.getAllWindows().length === 0) {
+        createWindow();
+    }
+});
 
-app.whenReady().then(createWindow)
+app.whenReady().then(createWindow);
